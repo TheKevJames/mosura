ca44d9ef071ab48d3d2027fa8fa99232319240be
Kevin James <KevinJames@thekev.in>
2022-10-12 13:15:26 +0100

    tests: fixup ping return code test

--- tests/app_test.py
+++ tests/app_test.py
@@ -4,5 +4,4 @@ import httpx
 async def test_root(client: httpx.AsyncClient) -> None:
     response = await client.get('/api/v0/ping')
 
-    assert response.status_code == 200
-    assert response.json() == {'status': 'ok'}
+    assert response.status_code == 204
